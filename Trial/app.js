2fa155a273c923feb462aeffa0eeef9514dbf427
Rushi <rushikeshdantakalecoding@gmail.com>
2022-07-31 23:15:29 +0530

    changes like ui changes and all stuff
    inside triaL I CHANDED SOME UI INTERFACES AND mainly i included jquery and gave a profile icon with deop down with no content...

--- Trial/app.js
+++ Trial/app.js
@@ -1,1 +1,13 @@
 
+
+var Toggle = $("#btnToggle");
+var hideList =$(".hideList");
+
+Toggle.click(function(){
+
+  if(hideList.css("display")=='none'){
+    hideList.css('display','block');
+  }else{
+    hideList.css('display','none');
+  }
+});
